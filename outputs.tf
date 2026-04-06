57fe8cf767878a80bef09b44278792d9ba85c38f
Judykenny <meetkennethjudith24@gmail.com>
2026-04-06 08:19:56 +0100

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-output "load_balancer_dns" {
-  value = aws_lb.main.dns_name
-}
-output "bastion_public_ip" {
-  value = aws_eip.bastion.public_ip
-}
